d0cfd30b8daa50e5bb285fa97da92b5e81ade637
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2020-11-27 20:28:23 -0300

    SocialLinks

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import * as S from "./styled" 
 
 import Profile from "../Profile"
+import SocialLinks from "../SocialLinks"
 
 const Sidebar = () => (
   <S.SidebarWrapper>
     <Profile />
+    <SocialLinks />
   </S.SidebarWrapper>
 )
  
