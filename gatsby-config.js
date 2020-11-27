b92bb13c2723357972d6610f5e998f7b35fd1de0
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2020-11-27 15:33:07 -0300

    StaticQuery e useStaticQuery

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Simple studant blog`,
+    description: `Este projeto foi criado como plataforma de compartilhamento de informações referentes ao conteúdo de encino médio `,
+    author: `@Marcus_Vinicius`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { graphql, useStaticQuery } from "gatsby"
 
-const Profile = () => (
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, description, author },
+    }
+  } = useStaticQuery(
+    graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+        }
+      }
+    }`
+  )
+  return (
   <div className="Profile-wrapper">
-    <h1>Marcus Vinicius</h1>
-    <h2>Programador full-stack</h2>
-    <p> Tenho 16 ano e estou no ensino médio. Meus conhecimentos abrangem Python, Java Script, HTML, CSS(SCSS), React, Arduino, Photoshop, GIMP e Vegas Pro, nos quais eu sou iniciante. Estou buscando evoluir no aprendizado e determinado a contribuir com um bom projeto.</p>
+    <h1>{title}</h1>
+    <h2>{description}</h2>
+    <p>{author}</p>
   </div>
-)
+  )
+}
  
 export default Profile;
